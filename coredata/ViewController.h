ba589ff30ea7a10fc72a53922c810ec1e6d220a0
wire <admin@macmini4>
2015-11-18 10:12:54 +0200

    Initial Commit

---
+++ coredata/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  coredata
+//
+//  Created by Admin on 11/18/15.
+//  Copyright © 2015 Wire IT College. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ coredata/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  coredata
+//
+//  Created by Admin on 11/18/15.
+//  Copyright © 2015 Wire IT College. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
